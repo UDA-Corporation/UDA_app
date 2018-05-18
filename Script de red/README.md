a27612ccc7c222411f0795f414ade9ad9e185be8
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2018-05-18 13:06:36 +0200

    Script mejorado

--- Script de red/README.md
+++ Script de red/README.md
@@ -1,11 +1,8 @@
 Estructura del fichero de log
 ====
-Nuestro planteamiento es ejecutar un ping, creado en un script bash, ejecutado en el servidor que realice un ping a cada nodo de la red (todos los contenedores que hay en proxmox), enviando 4 paquetes, y, que se ejecute cada medio minuto. En caso de que todos los nodos tengan conexión, se generará un fichero que indique que la conexión ha sido correcta, fecha, número de nodo y hora. Se irá añadiendo cada ping exitoso al fichero log, en bloques de 5 (existen 5 nodos, 5 contenedores). Para llevar un historial.  
-En cuanto haya un error de conexión en uno de los nodos, se mostrará inmediatamente en la terminal un mensaje que indica el nodo que da problemas, a la vez que otro pidiendo al usuario que mire el fichero “log” para más detalles.  
-Es decir, el fichero log contendrá un historial de los pings exitosos y fallidos, pero solo notificará al usuario cuando encuentre un problema con uno de los nodos. La estructura del fichero “log” será así:  
->#Estructura de error (no respuesta)    
+Nuestro planteamiento es ejecutar un ping, creado en un script bash, a cada nodo de la red y el de google (todos los contenedores que hay en proxmox y 8.8.8.8), enviando 4 paquetes, y, que se ejecute cada 10 minutos automáticamente desde que se enciende el contenedor y se inicia sesión.  
+En cuanto haya un error de conexión en uno de los nodos, se creará inmediatamente en la terminal un mensaje que indique el nodo que da problemas. Si en la próxima prueba (cada 10 minutos) hay conexion a internet, se eliminará el fichero log. Se crea y se mantiene solo mientras hay problemas en 1 o más nodos.  
+La estructura del fichero “log” será así:  
+<br/>
+>#Estructura de error (no respuesta) dentro del LOG   
 >Nodo: $node - 2018-04-19 11:02:34 - Failed - Check status of the node $node!  
->#La consola avisará al usuario de la siguiente forma  
->Error in the connection of node $node, check "ConnectionResults.log" for more details  
->#Estructura de conexion satisfactoria  
->Nodo: $node - 2018-04-19 11:02:34 - Successful  
--- Script de red/Script.sh
+++ Script de red/Script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#To repeat the script---> crontab -e (to open crontab) and put    in the file. Save.
+#To repeat the script automatically every 10 minutes---> crontab -e (to open crontab) and put "*/10 * * * * /root/Script/Script.sh" in the file. Save.
 echo "
 ▒█▄░▒█ █▀▀ ▀▀█▀▀ █░░░█ █▀▀█ █▀▀█ █░█ 　 █▀▀ █░░█ █▀▀ █▀▀ █░█ █▀▀ █▀▀█
 ▒█▒█▒█ █▀▀ ░░█░░ █▄█▄█ █░░█ █▄▄▀ █▀▄ 　 █░░ █▀▀█ █▀▀ █░░ █▀▄ █▀▀ █▄▄▀
@@ -7,14 +7,11 @@ echo "
 
 By UDA Corp. under MIT license
 "
-echo "The results will be saved in "ConnectionResults.log""
 #Indicamos cual sera el fichero log y le anadimos cabecera
 LOG="ConnectionResults.log"
-echo "
-Result(s) of the test(s)
+#Variable para borrar el log (todo conectado de nuevo)
+VAR=0
 
-IMPORTANT: If you are seeing this because it popped up asking you for it, check the last lines of this file to see the nodes affected
-" >> $LOG
 #Nodos para hacer ping
 TARGET="10.1.1.1"
 TARGET2="10.1.2.1"
@@ -22,59 +19,69 @@ TARGET3="10.1.3.1"
 TARGET4="10.1.11.1"
 TARGET5="10.1.10.1"
 TARGET6="8.8.8.8"
-#Hacemos el ping e indicamos si ha habido conexion o no a cada nodo
+
+#Hacemos el ping e indicamos si hay problemas en la conexion. Si la conexion funciona, elimina el fichero log.
 if ping -n -c 4 $TARGET >/dev/null #Ping de 4 paquetes
     then
-        #Hay conexion, se guarda en el log el mensaje
-        echo "Node: $TARGET - $(date +%F\ %T) - Successful" >> $LOG
+        VAR=`expr $VAR + 1`
     else
-        #No hay conexion y mostramos el aviso al usuario para que mire el log
+        #No hay conexion, creamos el archivo log
+        echo "
+        Result(s) of the test(s)
+        " >> $LOG
         echo "Node: $TARGET - $(date +%F\ %T) - Failed - Check status of the node $TARGET!" >> $LOG
-        echo "Error in the connection of node $TARGET, check "ConnectionResults.log" for more details"
 fi
 if ping -n -c 4 $TARGET2 >/dev/null #Ping de 4 paquetes
     then
-        #Hay conexion, se guarda en el log el mensaje
-        echo "Node: $TARGET2 - $(date +%F\ %T) - Successful" >> $LOG
+        VAR=`expr $VAR + 1`
     else
-        #No hay conexion y mostramos el aviso al usuario para que mire el log
+        #No hay conexion, creamos el archivo log
+        echo "
+        Result(s) of the test(s)
+        " >> $LOG
         echo "Node: $TARGET2 - $(date +%F\ %T) - Failed - Check status of the node $TARGET2!" >> $LOG
-        echo "Error in the connection of node $TARGET2, check "ConnectionResults.log" for more details"
 fi
 if ping -n -c 4 $TARGET3 >/dev/null #Ping de 4 paquetes
     then
-        #Hay conexion, se guarda en el log el mensaje
-        echo "Node: $TARGET3 - $(date +%F\ %T) - Successful" >> $LOG
+        VAR=`expr $VAR + 1`
     else
-        #No hay conexion y mostramos el aviso al usuario para que mire el log
+        #No hay conexion, creamos el archivo log
+        echo "
+        Result(s) of the test(s)
+        " >> $LOG
         echo "Node: $TARGET3 - $(date +%F\ %T) - Failed - Check status of the node $TARGET3!" >> $LOG
-        echo "Error in the connection of node $TARGET3, check "ConnectionResults.log" for more details"
 fi
 if ping -n -c 4 $TARGET4 >/dev/null #Ping de 4 paquetes
     then
-        #Hay conexion, se guarda en el log el mensaje
-        echo "Node: $TARGET4 - $(date +%F\ %T) - Successful" >> $LOG
+        VAR=`expr $VAR + 1`
     else
-        #No hay conexion y mostramos el aviso al usuario para que mire el log
+        #No hay conexion, creamos el archivo log
+        echo "
+        Result(s) of the test(s)
+        " >> $LOG
         echo "Node: $TARGET4 - $(date +%F\ %T) - Failed - Check status of the node $TARGET4!" >> $LOG
-        echo "Error in the connection of node $TARGET4, check "ConnectionResults.log" for more details"
 fi
 if ping -n -c 4 $TARGET5 >/dev/null #Ping de 4 paquetes
     then
-        #Hay conexion, se guarda en el log el mensaje
-        echo "Node: $TARGET5 - $(date +%F\ %T) - Successful" >> $LOG
+        VAR=`expr $VAR + 1`
     else
-        #No hay conexion y mostramos el aviso al usuario para que mire el log
+        #No hay conexion, creamos el archivo log
+        echo "
+        Result(s) of the test(s)
+        " >> $LOG
         echo "Node: $TARGET5 - $(date +%F\ %T) - Failed - Check status of the node $TARGET5!" >> $LOG
-        echo "Error in the connection of node $TARGET5, check "ConnectionResults.log" for more details"
 fi
 if ping -n -c 4 $TARGET6 >/dev/null #Ping de 4 paquetes
     then
-        #Hay conexion, se guarda en el log el mensaje
-        echo "Node: $TARGET6 - $(date +%F\ %T) - Successful" >> $LOG
+        VAR=`expr $VAR + 1`
     else
-        #No hay conexion y mostramos el aviso al usuario para que mire el log
-        echo "Node: $TARGET6 (Google) - $(date +%F\ %T) - Failed - Check status of the internet connection!" >> $LOG
-        echo "Error in the connection of node $TARGET6, check "ConnectionResults.log" for more details"
+        #No hay conexion, creamos el archivo log
+        echo "
+        Result(s) of the test(s)
+        " >> $LOG
+        echo "Node: $TARGET6 (Google)- $(date +%F\ %T) - Failed - Check status of the internet connection!" >> $LOG
 fi
-echo " " >>$LOG
+if [ "$VAR" -eq 6 ]
+    then
+        rm ConnectionResults.log
+fi
